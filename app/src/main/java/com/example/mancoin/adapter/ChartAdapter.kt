84e41d9f3ee830830a0171df94fdf6f98adadf46
mamad <omidikotlin@gmail.com>
2024-12-20 18:06:25 +0330

    chart coins added and implemented

---
+++ app/src/main/java/com/example/mancoin/adapter/ChartAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.mancoin.adapter
+
+import com.example.mancoin.data.ChartData
+import com.robinhood.spark.SparkAdapter
+
+class ChartAdapter (
+    private val historicalData : List<ChartData.Data>,
+    private val baseLine : String?) : SparkAdapter(){
+
+    override fun getCount(): Int {
+        return historicalData.size
+    }
+
+    override fun getItem(index: Int): ChartData.Data {
+        return historicalData[index]
+    }
+
+    override fun getY(index: Int): Float {
+        return historicalData[index].close.toFloat()
+    }
+
+    override fun hasBaseLine(): Boolean {
+        return true
+    }
+
+    override fun getBaseLine(): Float {
+        return baseLine?.toFloat() ?: super.getBaseLine()
+    }
+
+}
